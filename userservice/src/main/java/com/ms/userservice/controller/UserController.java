4dfee791a450eb56f465a097f3a7f54115b0494f
alexandergou <395655863@qq.com>
2019-09-12 03:18:39 +0800

    integration elk and user-service\

--- userservice/src/main/java/com/ms/userservice/controller/UserController.java
+++ userservice/src/main/java/com/ms/userservice/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ms.userservice.controller;
 
 import com.ms.userservice.entity.UserInfo;
 import com.ms.userservice.jpa.UserInfoJpa;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -24,6 +27,7 @@ public class UserController {
     }
     @PostMapping("/user")
     public ResponseEntity<UserInfo> addUser(@RequestBody UserInfo userInfo) {
+        log.info("add user start: {}",userInfo);
         userInfo.setId(UUID.randomUUID().toString());
         return ResponseEntity.ok(userRepo.save(userInfo));
     }
